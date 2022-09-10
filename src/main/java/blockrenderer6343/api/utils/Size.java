ba20439391cb77ca683cbe199da0eb9b5da9247e
くぁーりぃ <kaito6343@gmail.com>
2022-09-10 23:21:30 +0900

    add positionedrect

---
+++ src/main/java/blockrenderer6343/api/utils/Size.java
@@ -0,0 +1,46 @@
+package blockrenderer6343.api.utils;
+
+import java.util.Objects;
+
+public class Size {
+
+    public static final Size ZERO = new Size(0, 0);
+
+    public final int width;
+    public final int height;
+
+    public Size(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Size)) return false;
+        Size size = (Size) o;
+        return width == size.width &&
+            height == size.height;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(width, height);
+    }
+
+    @Override
+    public String toString() {
+        return com.google.common.base.Objects.toStringHelper(this)
+            .add("width", width)
+            .add("height", height)
+            .toString();
+    }
+}

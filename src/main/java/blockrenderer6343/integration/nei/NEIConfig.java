e0b29c8cf9b73e614b2dab6f927b3ff30d0a8bf1
__felix__ <134875831+felixfour@users.noreply.github.com>
2026-07-29 07:45:52 -0400

    rename

--- src/main/java/blockrenderer6343/integration/nei/NEIConfig.java
+++ src/main/java/blockrenderer6343/integration/nei/NEIConfig.java
@@ -24,7 +24,7 @@ public class NEIConfig implements IConfigureNEI {
 
     @SubscribeEvent
     public static void registerHandler(NEIRegisterHandlerInfosEvent event) {
-        if (BlockRenderer6343.isGTLoaded) {
+        if (BlockRenderer6343.isGT5uNHLoaded) {
             event.registerHandlerInfo(
                     new HandlerInfo.Builder(GTNEIMultiblockHandler.class, "GregTech", "gregtech")
                             .setDisplayStack(new ItemStack(StructureLibAPI.getDefaultHologramItem())).setHeight(168)
